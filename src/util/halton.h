80d5369aca0546ba8fa5731d68cd278e7fed450c
Williscool13 <twtw40@gmail.com>
2024-11-04 16:02:44 +0700

    Inlined Halton generator

--- src/util/halton.h
+++ src/util/halton.h
@@ -7,7 +7,8 @@
 
 #include <glm/glm.hpp>
 
-class HaltonSequence {
+class HaltonSequence
+{
 public:
     /**
      * Get 2D jitter offset for given frame index, scaled to pixel size
@@ -15,8 +16,14 @@ public:
      * @param screenDims
      * @return
      */
-    static glm::vec2 getJitter(uint32_t frameIndex, const glm::vec2& screenDims);
+    static glm::vec2 getJitter(uint32_t frameIndex, const glm::vec2& screenDims)
+    {
+        float x = halton(frameIndex, 2);
+        float y = halton(frameIndex, 3);
 
+        // Scale to screen dims
+        return {(x - 0.5f) / screenDims.x, (y - 0.5f) / screenDims.y};
+    }
 
 private:
     /**
@@ -26,14 +33,24 @@ private:
      * @param base
      * @return
      */
-    static float halton(uint32_t index, uint32_t base);
+    static float halton(uint32_t index, uint32_t base)
+    {
+        float f = 1.0f;
+        float r = 0.0f;
 
+        index = (index + 1); // Start from index 1 to avoid (0,0) jitter
+        while (index > 0) {
+            f /= static_cast<float>(base);
+            r += f * static_cast<float>(index % base);
+            index /= base;
+        }
+
+        return r;
+    }
 };
 
-class halton {
-
-};
-
+class halton
+{};
 
 
 #endif //HALTON_H
