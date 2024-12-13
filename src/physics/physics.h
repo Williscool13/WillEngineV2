28207577a53b3cce3fa3dad78847f1473d52bdf2
Williscool13 <twtw40@gmail.com>
2024-12-13 18:40:52 +0700

    Physics setup, basic box primitive falling onto world floor.

--- src/physics/physics.h
+++ src/physics/physics.h
@@ -11,7 +11,15 @@
 #include <Jolt/Physics/PhysicsSystem.h>
 #include <Jolt/Core/JobSystemThreadPool.h>
 
-namespace JPH {
+class PhysicsBody;
+class GameObject;
+
+namespace JPH
+{
+    class CylinderShape;
+    class CapsuleShape;
+    class SphereShape;
+    class BoxShape;
     class ContactListener;
     class BodyActivationListener;
     class BroadPhaseLayerInterface;
@@ -19,22 +27,30 @@ namespace JPH {
     class ObjectLayerPairFilter;
 }
 
-class Physics {
+class Physics
+{
 public:
     Physics();
+
     ~Physics();
 
-    void init();
     void cleanup();
-    void update(float deltaTime);
+
+    void update(float deltaTime) const;
 
     [[nodiscard]] JPH::PhysicsSystem& getPhysicsSystem() const { return *physicsSystem; }
-    JPH::BodyInterface& getBodyInterface();
+
+    JPH::BodyInterface& getBodyInterface() const;
 
 
-    // void addRigidBody(GameObject* obj, const JPH::Shape* shape, bool isDynamic = true);
-    // void removeRigidBody(GameObject* obj);
-    // void updateTransforms();
+    void addRigidBody(GameObject* obj, const JPH::ShapeRefC& shape, bool isDynamic = true);
+
+    void removeRigidBody(const GameObject* object);
+
+    void removeRigidBodies(const std::vector<GameObject>& objects);
+
+    void updateTransforms() const;
+
 private:
     // Core systems
     JPH::PhysicsSystem* physicsSystem = nullptr;
@@ -54,11 +70,25 @@ private:
     static constexpr uint32_t MAX_BODIES = 10240;
     static constexpr uint32_t MAX_BODY_PAIRS = 10240;
     static constexpr uint32_t MAX_CONTACT_CONSTRAINTS = 10240;
-    static constexpr uint32_t NUM_BODY_MUTEXES = 0;  // 0 for default
-    static constexpr uint32_t TEMP_ALLOCATOR_SIZE = 10 * 1024 * 1024;  // 10MB
+    static constexpr uint32_t NUM_BODY_MUTEXES = 0; // 0 for default
+    static constexpr uint32_t TEMP_ALLOCATOR_SIZE = 10 * 1024 * 1024; // 10MB
+
+    static constexpr int32_t WORLD_FLOOR_HEIGHT = -20;
+
+    std::unordered_map<uint32_t, PhysicsBody> physicsBodies;
+
+public:
+    JPH::ShapeRefC getUnitCubeShape() const { return unitCubeShape; }
+    JPH::ShapeRefC getUnitSphereShape() const { return unitSphereShape; }
+    JPH::ShapeRefC getUnitCapsuleShape() const { return unitCapsuleShape; }
+    JPH::ShapeRefC getUnitCylinderShape() const { return unitCylinderShape; }
+
+private: // Shapes
+    JPH::ShapeRefC unitCubeShape = nullptr;
+    JPH::ShapeRefC unitSphereShape = nullptr;
+    JPH::ShapeRefC unitCapsuleShape = nullptr;
+    JPH::ShapeRefC unitCylinderShape = nullptr;
 };
-
-
 
 
 #endif //PHYSICS_H
---
+++ src/physics/physics_utils.cpp
@@ -0,0 +1,5 @@
+//
+// Created by William on 2024-12-13.
+//
+
+#include "physics_utils.h"
