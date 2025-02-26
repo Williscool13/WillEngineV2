9022dd5148e77a7e3a1a6df5bb294cb82a752f48
Williscool13 <twtw40@gmail.com>
2025-02-26 22:38:22 +0700

    Terrain shaders.

--- src/renderer/pipelines/terrain/terrain_pipeline.cpp
+++ src/renderer/pipelines/terrain/terrain_pipeline.cpp
@@ -3,3 +3,144 @@
 //
 
 #include "terrain_pipeline.h"
+
+#include <volk/volk.h>
+
+#include "src/renderer/renderer_constants.h"
+#include "src/renderer/terrain/terrain_types.h"
+
+will_engine::terrain::TerrainPipeline::TerrainPipeline(ResourceManager& resourceManager) : resourceManager(resourceManager)
+{}
+
+will_engine::terrain::TerrainPipeline::~TerrainPipeline()
+{
+    resourceManager.destroyPipelineLayout(pipelineLayout);
+    resourceManager.destroyPipeline(pipeline);
+}
+
+void will_engine::terrain::TerrainPipeline::draw(VkCommandBuffer cmd, const TerrainDrawInfo& drawInfo) const
+{
+    VkDebugUtilsLabelEXT label = {};
+    label.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_LABEL_EXT;
+    label.pLabelName = "Deferred MRT Pass (Terrain)";
+    vkCmdBeginDebugUtilsLabelEXT(cmd, &label);
+
+    VkClearValue clearValue = {0.0f, 0.0f};
+
+    VkRenderingAttachmentInfo normalAttachment = vk_helpers::attachmentInfo(drawInfo.normalTarget, drawInfo.bClearColor ? &clearValue : nullptr, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);
+    VkRenderingAttachmentInfo albedoAttachment = vk_helpers::attachmentInfo(drawInfo.albedoTarget, drawInfo.bClearColor ? &clearValue : nullptr, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);
+    VkRenderingAttachmentInfo pbrAttachment = vk_helpers::attachmentInfo(drawInfo.pbrTarget, drawInfo.bClearColor ? &clearValue : nullptr, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);
+    VkRenderingAttachmentInfo velocityAttachment = vk_helpers::attachmentInfo(drawInfo.velocityTarget, drawInfo.bClearColor ? &clearValue : nullptr, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);
+    VkRenderingAttachmentInfo depthAttachment = vk_helpers::attachmentInfo(drawInfo.depthTarget, drawInfo.bClearColor ? &clearValue : nullptr, VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL);
+
+    VkRenderingInfo renderInfo{};
+    renderInfo.sType = VK_STRUCTURE_TYPE_RENDERING_INFO;
+    renderInfo.pNext = nullptr;
+
+    VkRenderingAttachmentInfo deferredAttachments[4];
+    deferredAttachments[0] = normalAttachment;
+    deferredAttachments[1] = albedoAttachment;
+    deferredAttachments[2] = pbrAttachment;
+    deferredAttachments[3] = velocityAttachment;
+
+    renderInfo.renderArea = VkRect2D{VkOffset2D{0, 0}, RENDER_EXTENTS};
+    renderInfo.layerCount = 1;
+    renderInfo.colorAttachmentCount = 4;
+    renderInfo.pColorAttachments = deferredAttachments;
+    renderInfo.pDepthAttachment = &depthAttachment;
+    renderInfo.pStencilAttachment = nullptr;
+
+    vkCmdBeginRendering(cmd, &renderInfo);
+    vkCmdBindPipeline(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline);
+
+    //  Viewport
+    VkViewport viewport = {};
+    viewport.x = 0;
+    viewport.y = 0;
+    viewport.width = drawInfo.viewportExtents.x;
+    viewport.height = drawInfo.viewportExtents.y;
+    viewport.minDepth = 0.f;
+    viewport.maxDepth = 1.f;
+    vkCmdSetViewport(cmd, 0, 1, &viewport);
+    //  Scissor
+    VkRect2D scissor = {};
+    scissor.offset.x = 0;
+    scissor.offset.y = 0;
+    scissor.extent.width = RENDER_EXTENTS.width;
+    scissor.extent.height = RENDER_EXTENTS.height;
+    vkCmdSetScissor(cmd, 0, 1, &scissor);
+
+    constexpr VkDeviceSize zeroOffset{0};
+
+    // for terrain chunk : chunks { draw }
+    // for (RenderObject* val : drawInfo.renderObjects | std::views::values) {
+    //     const RenderObject* renderObject = val;
+    //     if (!renderObject->canDraw()) { continue; }
+    //
+    //     constexpr uint32_t sceneDataIndex{0};
+    //     constexpr uint32_t addressIndex{1};
+    //     constexpr uint32_t texturesIndex{2};
+    //
+    //     VkDescriptorBufferBindingInfoEXT descriptorBufferBindingInfo[3];
+    //     descriptorBufferBindingInfo[0] = drawInfo.sceneDataBinding;
+    //     descriptorBufferBindingInfo[1] = renderObject->getAddressesDescriptorBuffer().getDescriptorBufferBindingInfo();
+    //     descriptorBufferBindingInfo[2] = renderObject->getTextureDescriptorBuffer().getDescriptorBufferBindingInfo();
+    //     vkCmdBindDescriptorBuffersEXT(cmd, 3, descriptorBufferBindingInfo);
+    //
+    //     const VkDeviceSize sceneDataOffset{drawInfo.sceneDataOffset};
+    //     const VkDeviceSize addressOffset{renderObject->getAddressesDescriptorBuffer().getDescriptorBufferSize() * drawInfo.currentFrameOverlap};
+    //
+    //     vkCmdSetDescriptorBufferOffsetsEXT(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 0, 1, &sceneDataIndex, &sceneDataOffset);
+    //     vkCmdSetDescriptorBufferOffsetsEXT(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 1, 1, &addressIndex, &addressOffset);
+    //     vkCmdSetDescriptorBufferOffsetsEXT(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 2, 1, &texturesIndex, &zeroOffset);
+    //
+    //     vkCmdBindVertexBuffers(cmd, 0, 1, &renderObject->getVertexBuffer().buffer, &zeroOffset);
+    //     vkCmdBindIndexBuffer(cmd, renderObject->getIndexBuffer().buffer, 0, VK_INDEX_TYPE_UINT32);
+    //     vkCmdDrawIndexedIndirect(cmd, renderObject->getIndirectBuffer(drawInfo.currentFrameOverlap).buffer, 0, renderObject->getDrawIndirectCommandCount(), sizeof(VkDrawIndexedIndirectCommand));
+    // }
+
+    vkCmdEndRendering(cmd);
+
+    vkCmdEndDebugUtilsLabelEXT(cmd);
+}
+
+void will_engine::terrain::TerrainPipeline::createPipeline()
+{
+    resourceManager.destroyPipeline(pipeline);
+    VkShaderModule vertShader = resourceManager.createShaderModule("shaders/terrain/terrain.vert");
+    VkShaderModule fragShader = resourceManager.createShaderModule("shaders/terrain/terrain.frag");
+
+    PipelineBuilder pipelineBuilder;
+    VkVertexInputBindingDescription mainBinding{};
+    mainBinding.binding = 0;
+    mainBinding.stride = sizeof(TerrainVertex);
+    mainBinding.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+    VkVertexInputAttributeDescription vertexAttributes[3];
+    vertexAttributes[0].binding = 0;
+    vertexAttributes[0].location = 0;
+    vertexAttributes[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+    vertexAttributes[0].offset = offsetof(TerrainVertex, position);
+    vertexAttributes[1].binding = 0;
+    vertexAttributes[1].location = 0;
+    vertexAttributes[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+    vertexAttributes[1].offset = offsetof(TerrainVertex, normal);
+    vertexAttributes[2].binding = 0;
+    vertexAttributes[2].location = 0;
+    vertexAttributes[2].format = VK_FORMAT_R32G32B32_SFLOAT;
+    vertexAttributes[2].offset = offsetof(TerrainVertex, uv);
+
+    pipelineBuilder.setupVertexInput(&mainBinding, 1, vertexAttributes, 1);
+
+    pipelineBuilder.setShaders(vertShader, fragShader);
+    pipelineBuilder.setupInputAssembly(VK_PRIMITIVE_TOPOLOGY_TRIANGLE_STRIP, true);
+    pipelineBuilder.setupRasterization(VK_POLYGON_MODE_FILL, VK_CULL_MODE_NONE, VK_FRONT_FACE_CLOCKWISE);
+    pipelineBuilder.disableMultisampling();
+    pipelineBuilder.setupBlending(PipelineBuilder::BlendMode::NO_BLEND);
+    pipelineBuilder.enableDepthTest(true, VK_COMPARE_OP_GREATER_OR_EQUAL);
+    pipelineBuilder.setupRenderer({NORMAL_FORMAT, ALBEDO_FORMAT, PBR_FORMAT, VELOCITY_FORMAT}, DEPTH_FORMAT);
+    pipelineBuilder.setupPipelineLayout(pipelineLayout);
+
+    pipeline = resourceManager.createRenderPipeline(pipelineBuilder, {VK_DYNAMIC_STATE_DEPTH_BIAS});
+    resourceManager.destroyShaderModule(vertShader);
+    resourceManager.destroyShaderModule(fragShader);
+}
